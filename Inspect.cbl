c553a3cfb7e3511da91f64b951abc95182035acf
Darwin Nebit <chaseboy006@yahoo.com>
2016-04-30 12:12:23 +0800

    Update Inspect.cbl

--- Inspect.cbl
+++ Inspect.cbl
@@ -4,9 +4,9 @@
  Data Division.
  Working-Storage Section.
  01   Phone-number.
- 03  Area-code       Pic XXX value "409".
- 03  Prefix-num      Pic XXX value "555".
- 03  Last-four       Pic X(4)  value "1212".
+    03  Area-code       Pic XXX value "409".
+    03  Prefix-num      Pic XXX value "555".
+    03  Last-four       Pic X(4)  value "1212".
  01   Formatted-Number      Pic X(14) value "(XXX) YYY-ZZZZ".
  01   Formatted-Alternate   Pic X(14) value "(XXX) XXX-XXXX".
  Procedure Division.
@@ -17,8 +17,13 @@
                 All "ZZZZ" by Last-four.
     Display Formatted-Number.
     Inspect Formatted-Alternate
-        Replacing First "XXX" by Area-code
+        Replacing FIRST "XXX" by Area-code
                 First "XXX" by Prefix-num
                 First "XXXX" by Last-four.
     Display Formatted-Alternate.
+
+    Inspect FORMATTED-ALTERNATE 
+        Replacing ALL "-" by "*".
+
+    DISPLAY FORMATTED-ALTERNATE.
     Stop Run.
